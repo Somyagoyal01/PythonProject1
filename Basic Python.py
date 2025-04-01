a5160c959367507341c68cf092badc3c1422befb
Somya Goyal <jkgsomya12@gmail.com>
2025-04-01 10:05:58 +0530

    Initial commit

---
+++ Basic Python.py
@@ -0,0 +1,16 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen."""
+Operand_1=int(input("Enter your first number="))
+Operand_2=int(input("Enter your second number="))
+print("Addition=",Operand_1+Operand_2)
+print("Subtraction=",Operand_1-Operand_2)
+print("Multiplication=",Operand_1*Operand_2)
+print("Division=",Operand_1/Operand_2)
+
